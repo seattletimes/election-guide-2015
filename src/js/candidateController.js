52b07b64e39abc4e522186fa93c3ca250917ab55
Thomas Wilburn <news@thomaswilburn.net>
2015-09-30 09:35:11 -0700

    URL updates when switching candidates

--- src/js/candidateController.js
+++ src/js/candidateController.js
@@ -1,6 +1,6 @@
 var data = require("./guideData");
 
-var candidateController = function($scope, $state) {
+var candidateController = function($scope, $state, $location) {
 
   $scope.candidates = data.candidates.all;
   $scope.questions = data.questions;
@@ -10,8 +10,14 @@ var candidateController = function($scope, $state) {
     $scope.selected = data.candidates.lookup[nameParam];
   }
 
+  $scope.$watch("selected", function() {
+    if (!$scope.selected) return;
+    //update the URL
+    $state.go("candidates", { name: $scope.selected ? $scope.selected.href : "" }, { notify: false });
+  });
+
 };
 
-candidateController.$inject = ["$scope", "$state"];
+candidateController.$inject = ["$scope", "$state", "$location"];
 
 module.exports = candidateController;
