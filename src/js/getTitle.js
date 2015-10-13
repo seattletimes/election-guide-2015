e664e0dbd710a7f6052d9af4c0949f6b9f124b6c
Thomas Wilburn <news@thomaswilburn.net>
2015-10-13 11:20:18 -0700

    Better titles in "running for" blurb

--- src/js/getTitle.js
+++ src/js/getTitle.js
@@ -1,12 +1,21 @@
+var locations = {
+  bellevue: "Bellevue City Council",
+  king: "King County",
+  seattle: "Seattle City Council",
+  schoolBoard: "Seattle School Board",
+  port: "Port of Seattle"
+}
+
 var getTitle = function(locale, position) {
+  var l = locations[locale];
   if (["bellevue", "king", "schoolBoard"].indexOf(locale) > -1) {
-    return "District #" + position;
+    return l + " District #" + position;
   }
   if (locale == "port") {
-    return "Seat #" + position;
+    return l + " Seat #" + position;
   }
   if (locale == "seattle") {
-    return position > 7 ? "Position #" + position : "District #" + position;
+    return position > 7 ? l + " Position #" + position : l + " District #" + position;
   }
   return position;
 }
